46fb8d2d8182eebfa8b52c9e659c6e314a3916e0
M00St4r <bjoern.martens@posteo.de>
2024-06-20 15:32:23 +0200

    fwwf

---
+++ Robjörn/main.js
@@ -0,0 +1,4 @@
+var message1 = prompt("Hello");
+if (message1 = "Hi") {
+    document.getElementById("Output").innerHTML = "how are you";
+}
---
+++ Robjörn/main.ts
@@ -0,0 +1,6 @@
+var message1 = prompt("Hello");
+if(message1 = "Hi"){
+
+    document.getElementById("Output").innerHTML = "how are you";
+
+}
