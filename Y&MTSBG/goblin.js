d79eeabadcbe74dbebbc71d5e9e7870124f21ac5
matbudimir <dotbudi@gmail.com>
2024-06-20 15:24:00 +0200

    js

--- Y&MTSBG/goblin.js
+++ Y&MTSBG/goblin.js
@@ -1,3 +1,4 @@
 "use strict";
 console.log("Hello!");
 let start = prompt("You are a young goblin boi, and it is a wonderfully dreary day in your moist cavern. Somehow, rays of light manage to reach the dark depths in which you slumber, prompting you to [wake up]. However, you are tired from the previous night, one full of debauchery, and you wish to keep [sleeping]", "What do you do?");
+let;
