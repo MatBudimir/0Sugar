26f2190a25068ba2ef8fa349632c2e5349adc12c
M00St4r <bjoern.martens@posteo.de>
2024-06-22 01:43:07 +0200

    fix delay and add breaks

--- Robjörn/main.js
+++ Robjörn/main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     var message = "";
     var timeoutHandeler;
+    var delay = 200;
     changeBG("1");
     function story(storyPart) {
         switch (storyPart) {
@@ -11,27 +12,32 @@ document.addEventListener("DOMContentLoaded", function () {
                         message = "";
                         clearTimeout(timeoutHandeler);
                         changeBG("2A");
+                        break;
                     }
                     else if (message == "b") {
                         message = "";
                         clearTimeout(timeoutHandeler);
                         changeBG("2B");
+                        break;
                     }
                 }
+                break;
             case "2A":
                 while (message == "") {
                     message = prompt("Level 2: Left Path\n As you walk along the left path, you come across a talking fox. The fox offers to guide you through the forest in exchange for a favor.\n Accept the Fox's Offer: You agree to the fox's terms, curious about the favor it might ask for later.\n Decline the Fox's Offer: You decide to trust your own instincts and continue walking alone");
                 }
+                break;
             case "2B":
                 while (message == "") {
                     message = prompt("Level 2: Right Path\n Option 2A: As you proceed down the right path, you encounter a group of goblins who seem to be guarding something precious.\n Confront the Goblins: You decide to face the goblins head-on and see what they are protecting.\n Sneak Past the Goblins: You try to sneak past them, hoping to avoid a confrontation.");
                 }
+                break;
         }
     }
     function changeBG(bgTag) {
         //let bgTag: string = bg.getBG();
         document.body.style.backgroundImage = "url('images/" + bgTag + ".jpg')";
-        timeoutHandeler = setTimeout(story, 1000, bgTag);
+        timeoutHandeler = setTimeout(story, delay, bgTag);
     }
 });
 //setTimeout(function(){
