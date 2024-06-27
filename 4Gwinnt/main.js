e5ef0cfb61fe9cf352a013efa32d81c91e26c36d
M00St4r <bjoern.martens@posteo.de>
2024-06-27 11:29:54 +0200

    update Code

--- 4Gwinnt/main.js
+++ 4Gwinnt/main.js
@@ -1,3 +1,4 @@
 var startButton = document.getElementById("startGame");
+startButton === null || startButton === void 0 ? void 0 : startButton.addEventListener("click", startGame);
 function startGame() {
 }
