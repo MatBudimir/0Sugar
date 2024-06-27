437c2448575163fc20d59235c506ef252a78817f
M00St4r <bjoern.martens@posteo.de>
2024-06-27 11:44:55 +0200

    update code

--- 4Gwinnt/main.js
+++ 4Gwinnt/main.js
@@ -1,4 +1,7 @@
 var startButton = document.getElementById("startGame");
 startButton === null || startButton === void 0 ? void 0 : startButton.addEventListener("click", startGame);
 function startGame() {
+    var gridCols = document.getElementById("cols");
+    var gridRows = document.getElementById("rows");
+    gridCols.innerHTML;
 }
--- 4Gwinnt/main.ts
+++ 4Gwinnt/main.ts
@@ -3,5 +3,8 @@ let startButton: HTMLElement = <HTMLElement>document.getElementById("startGame")
 startButton?.addEventListener("click", startGame);
 
 function startGame(){
-    
+    let gridCols: HTMLInputElement = <HTMLInputElement>document.getElementById("cols");
+    let gridRows: HTMLInputElement = <HTMLInputElement>document.getElementById("rows");
+
+    gridCols.innerHTML
 }
